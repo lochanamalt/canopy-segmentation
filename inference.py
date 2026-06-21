c707319d7088d774f705816dedaba35de74fb53c
Lochana Marasinghe <lochanatharanganie@gmail.com>
2026-06-21 00:33:58 -0700

    added inference for the test img

--- inference.py
+++ inference.py
@@ -12,22 +12,21 @@ from albumentations.pytorch import ToTensorV2
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-# 1. Reconstruct the Model
+# Reconstruct the Model
 model = smp.DeepLabV3Plus(
     encoder_name="mobilenet_v2",
-    encoder_weights=None,  # No need for imagenet weights since we are loading yours
+    encoder_weights=None,  # No need for imagenet weights
     in_channels=3,
     classes=1
 ).to(DEVICE)
 
-model.load_state_dict(torch.load("best_canopy_seg_model.pth", map_location=DEVICE))
+model.load_state_dict(torch.load("models/best_canopy_seg_model.pth", map_location=DEVICE))
 model.eval()
 
 transform = A.Compose([
     A.Normalize(),
     ToTensorV2(),
 ])
-
 
 def predict_canopy(image_path):
 
@@ -39,18 +38,18 @@ def predict_canopy(image_path):
     with torch.no_grad():
         output = model(input_tensor)
         # Apply Sigmoid to get probabilities (0.0 to 1.0)
-        mask = torch.sigmoid(output).squeeze().cpu().numpy()
+        canopy_mask = torch.sigmoid(output).squeeze().cpu().numpy()
 
-    binary_mask = (mask > 0.5).astype(np.uint8)
+    binary_mask = (canopy_mask > 0.5).astype(np.uint8)
     overlay = original_image.copy()
-    overlay[mask > 0.5] = [0, 255, 0]  # Green color
-    combined = cv2.addWeighted(original_image, 0.7, overlay, 0.3, 0)
+    overlay[canopy_mask > 0.5] = [0, 255, 0]  # Green color
+    combined_with_img = cv2.addWeighted(original_image, 0.7, overlay, 0.3, 0)
 
-    return binary_mask, combined
+    return binary_mask, combined_with_img
 
 
 # Usage
-mask, combined = predict_canopy("yolo_annotations/test/images/pi2_date_18-6-2025_15-0-10_1_png.rf.1c089affd68a1972231bdd3e3b8120cc.jpg")
-cv2.imwrite("output_mask.png", mask * 255)  # Multiply by 255 to make it visible
-cv2.imwrite("overlay.png", combined)
-# Create a green tint for the detected canopy
+mask, combined = predict_canopy("test_image/date_24-5-2025_10.0.10_1.png")
+cv2.imwrite("test_image/output_mask.png", mask * 255)  # Multiply by 255 to make it visible
+cv2.imwrite("test_image/overlay.png", combined)
+
